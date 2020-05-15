02ce3e9d37efc5c07c10418ef09ec23b3a92284b
Claudia Oprea <claudiaoprea16@gmail.com>
2020-05-15 18:15:12 +0300

    wip hiring page

---
+++ source/_partials/_hiring/hiring.blade.php
@@ -0,0 +1,67 @@
+<div class="rs-section rs-section-dark p-4">
+    <div class="container mx-auto">
+        <div class="w-full md:w-4/5 mx-auto">
+        <ul class="list-reset flex justify-center items-center text-xl md:text-2xl font-avenir text-uppercase px-8 sm:px-0">
+            <li class="">
+                <a href="/">@include('_partials.logo')</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</div>
+<div id="investor" class="rs-section rs-section-light px-8 md:px-0">
+    <div class="container mx-auto text-center">
+        <div class="rs-section-title" data-text="We're looking for talented people">
+            We're looking for talented people to join the team
+        </div>
+        <div class="rs-section-body">
+            <div class="w-full md:w-2/3 mx-auto mt-4">
+                <p>We are on a mission to develop the most user friendly and innovative fantasy games - connecting fantasy and sport as seamlessly as possible. We want to make sport even more exciting by creating the world's best fantasy games.</p>
+                <p>Our platform powers fantasy games on web and mobile apps. We are a small, friendly group passionate about fantasy and sports and we are really excited about growing the team and working with you. We started out with the idea of taking fantasy football to the next level and now in addition to running our own fantasy football site we’re delivering fantasy games to a multitude of companies and adding as many sports as we can to the platform.</p>
+                <p>Do you fancy working in an industry that reaches millions? This is a fantastic opportunity to be a part of an exciting journey - we believe that the world has only seen a fraction of the power fantasy games have. We see fantasy games becoming a natural part of any sport and we’re looking for the right people to help take fantasy games into unchartered territory.</p>
+            </div>
+
+            <div class="flex flex-wrap justify-center items-center mt-24">
+                <div class="sm:w-1/2 md:w-1/2 lg:w-1/4 mb-4 lg:mb-0">
+                    <span class="text-uppercase text-xl text-black block text-orange font-avenir">
+                        On a mission. As a team.
+                    </span>
+                    <span class="text-black block">
+                        We believe in working as a team towards a common goal. Collaboration, openness, inclusivity, transparency are just some of the values we hold highly.
+                    </span>
+                </div>
+                <div class="sm:w-1/2 md:w-1/2 lg:w-1/4 mb-4 lg:mb-0">
+                    <span class="text-uppercase text-xl text-black block text-orange font-avenir">
+                        Your growth. Our growth.
+                    </span>
+                    <span class="text-black block">
+                        Continual knowledge-sharing, learning and support benefits all of us. We expect you to constantly wish to up-skill, which we will gladly help with.
+                    </span>
+                </div>
+            </div>
+            
+            <div class="flex flex-wrap justify-center items-center mt-24">
+                <div class="sm:w-1/2 md:w-1/2 lg:w-1/4 mb-4 lg:mb-0">
+                    <span class="text-uppercase text-xl text-black block text-orange font-avenir">
+                        Engineering and Design. Together.
+                    </span>
+                    <span class="text-black block">
+                        We believe the best products are the ones that put the user at the forefront. Whether that's a developer using an API or a sports fan picking their team.
+                    </span>
+                </div>
+                <div class="sm:w-1/2 md:w-1/2 lg:w-1/4 mb-4 lg:mb-0">
+                    <span class="text-uppercase text-xl text-black block text-orange font-avenir">
+                        Make great stuff. Enjoy the process.
+                    </span>
+                    <span class="text-black block">
+                        Ultimately we want to make great things and we want to have fun doing it. We love working with smart people who are willing to challenge and debate.
+                    </span>
+                </div>
+            </div>
+            
+            <div class="flex flex-wrap justify-center items-center mt-24">
+                <script type="text/javascript" src="https://form.jotform.com/jsform/201333477258355"></script>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ source/hiring.blade.php
@@ -0,0 +1,7 @@
+@extends('_layouts.master')
+
+@section('body')
+    @include('_partials._hiring.hiring')
+    @include('_partials._home.contact')
+    @include('_partials._home.footer')
+@endsection
