cfba02452c23271eacb34c7facfbb90c0e2537bf
Oprea Claudia-Anamaria <claudiaoprea16@gmail.com>
2020-05-19 13:31:26 +0300

    [FT-3252] Hiring page (#15)
    * wip hiring page
    
    * Add job description
    
    * Change link to careers
    
    * remove f
    
    * Change career page with external link and values
    
    * update br

---
+++ source/careers.blade.php
@@ -0,0 +1,7 @@
+@extends('_layouts.master')
+
+@section('body')
+    @include('_partials._careers.careers')
+    @include('_partials._home.contact')
+    @include('_partials._home.footer')
+@endsection
