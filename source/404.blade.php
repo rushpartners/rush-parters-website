d7bc95afa6e074cdad48bfe04f34c049904e1309
Claudia Oprea <claudiaoprea16@gmail.com>
2020-10-22 16:10:01 +0300

    update font size

--- source/404.blade.php
+++ source/404.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('body')
-    @include('_partials._home.hero', ['error' => true])
+    @include('_partials._home.hero-error')
     @include('_partials._home.brands')
     @include('_partials._home.about')
     @include('_partials._home.contact')
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('body')
-    @include('_partials._home.hero', ['error' => false])
+    @include('_partials._home.hero')
     @include('_partials._home.brands')
     @include('_partials._home.about')
     @include('_partials._home.contact')
