21ce0b0a1b992a97e9131942310d3cd260494ba7
Oprea Claudia-Anamaria <claudiaoprea16@gmail.com>
2020-10-23 10:07:24 +0300

    [FT-3456] Create 404 page (#28)
    * Create 404 page
    
    * update font size

--- source/_partials/_home/hero-error.blade.php
+++ source/_partials/_home/hero-error.blade.php
@@ -1,4 +1,4 @@
-<div class="rs-hero px-8" style="background-image: url('./images/header-banner.png');">
+<div class="rs-hero px-8" style="background-image: url('../images/header-banner.png');">
     <div class="container mx-auto pt-8">
         <div class="w-full md:w-4/5 mx-auto">
             @include('_partials.navbar')
@@ -7,11 +7,11 @@
         <h2 class="rs-hero-headline" data-text="Feel the Rush">Feel the Rush</h2>
         <div class="w-full md:w-4/5 mx-auto relative z-40 flex mt-8 mb-16 sm:mb-0">
             <div class="text-xl text-white w-full md:w-4/5 lg:w-3/5 pr-0 sm:pr-8">
-                We believe in the power of Sports, the importance of the Fans and the thrill of the Game. We're on a mission to change the gambling industry, for good.
+                <h2>The page you are looking for <br />does not exist</h2>
             </div>
             <div class="w-2/5 text-right hidden lg:inline-block">
-                <a class="btn-cta" href="#our-brands" rush-navbar-item>
-                    Find Out More @include('_partials._icons.chevron-right')
+                <a class="btn-cta" href="/">
+                    Go Back @include('_partials._icons.chevron-right')
                 </a>
             </div>
         </div>
