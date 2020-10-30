67bd430e696e6fc4cb203b1c802d179b62a9b7de
Claudia Oprea <claudiaoprea16@gmail.com>
2020-10-30 18:10:48 +0200

    center links

--- source/_partials/_investor/investor.blade.php
+++ source/_partials/_investor/investor.blade.php
@@ -21,41 +21,41 @@
                 <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 mb-4 lg:mb-0">
                     <span class="text-uppercase text-xl text-black block text-orange font-avenir">30th of November 2020</span>
                     <span class="text-uppercase text-black block">Notice of extraordinary general meeting</span>
-                    <div class="text-left p-4">
+                    <div class="p-4">
                     <a 
                         target="_blank" 
                         href="https://s3-eu-west-1.amazonaws.com/www.rushpartners.co.uk/investor/Notice-AGM-Rush-Entertainment-Group-AB.pdf" 
                         class="p-1 text-uppercase block"
                     >
-                        1. Notice of annual general meeting
+                        Notice of annual general meeting
                     </a>
                     <a 
                         target="_blank" 
                         href="https://s3-eu-west-1.amazonaws.com/www.rushpartners.co.uk/investor/Formular-for-postrostning.pdf" 
                         class="p-1 text-uppercase block"
                     >
-                        2. FORMULÄR FÖR POSTRÖSTNING
+                        FORMULÄR FÖR POSTRÖSTNING
                     </a>
                     <a 
                         target="_blank" 
                         href="https://s3-eu-west-1.amazonaws.com/www.rushpartners.co.uk/investor/Postal-voting-form.pdf" 
                         class="p-1 text-uppercase block"
                     >
-                        3. POSTAL VOTING FORM
+                        POSTAL VOTING FORM
                     </a>
                     <a 
                         target="_blank" 
                         href="https://s3-eu-west-1.amazonaws.com/www.rushpartners.co.uk/investor/Fullmakt-med-rostinstruktioner.pdf" 
                         class="p-1 text-uppercase block"
                     >
-                        4. FULLMAKT FÖR OMBUD
+                        FULLMAKT FÖR OMBUD
                     </a>
                     <a 
                         target="_blank" 
                         href="https://s3-eu-west-1.amazonaws.com/www.rushpartners.co.uk/investor/proxy-form.pdf" 
                         class="p-1 text-uppercase block"
                     >
-                        5. PROXY FORM
+                        PROXY FORM
                     </a>
 </div>
                 </div>
