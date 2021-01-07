a47dda8a5e9209c37c039a6e109e50f88b59d61f
Claudia Oprea <claudiaoprea16@gmail.com>
2021-01-07 14:46:00 +0200

    Show current year @ copyright

--- source/_partials/_home/footer.blade.php
+++ source/_partials/_home/footer.blade.php
@@ -1,8 +1,15 @@
+<script>
+    window.onload = function myFunction() {
+        var date = new Date();
+        var year = date.getFullYear();
+        document.getElementById("year").innerHTML = year;
+    }
+</script>
 <div class="rs-section rs-section-darker py-6">
     <div class="container mx-auto">
         <div class="flex items-center flex-wrap content-center relative z-40">
             <div class="w-full md:w-1/2 text-grey text-center md:text-left mb-8 md:mb-0">
-                Copyright &copy; {{ date('Y') }} <span class="text-white">Rush Partners Ltd</span>. All rights reserved.
+                Copyright &copy; <span id="year"></span> <span class="text-white">Rush Partners Ltd</span>. All rights reserved.
             </div>
 
             <div class="w-full md:w-1/2 text-grey relative">
